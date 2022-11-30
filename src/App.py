ae1d2aac79396c041e817881267049ef100b6bf3
Keon Nguyen <cuongmits@gmail.com>
2022-11-30 21:44:56 +0100

    Create Menu & Pages

--- src/App.py
+++ src/App.py
@@ -12,10 +12,27 @@ import plotly.graph_objects as go
 
 #streamlit
 import streamlit as st
+from streamlit_option_menu import option_menu as om
 
 #others
 from copy import deepcopy
 import json
 
-st.sidebar.title("Control Panel")
 st.header("Migros Stores Consultancy in Switzerland")
+
+with st.sidebar:
+    selected = om(
+        'Navigation',
+        ['Home', 'Data Processing', 'Settings'],
+        icons=['house', 'cloud-download', 'gear'],
+        menu_icon='',
+        default_index=0,
+        orientation='vertical'
+    )
+
+if selected == 'Home':
+    exec(open('./src/Page/HomePage.py').read())
+elif selected == 'Data Processing':
+    exec(open('./src/Page/DataProcessingPage.py').read())
+else:
+    st.write('Settings Page - TODO')
