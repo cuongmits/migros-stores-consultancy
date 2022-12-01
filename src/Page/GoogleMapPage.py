254e856538724faf1cf84fa327ee445382f7cd5f
Keon Nguyen <cuongmits@gmail.com>
2022-12-02 00:29:48 +0100

    Build necessary dataframe with indicators & plotting

--- src/Page/GoogleMapPage.py
+++ src/Page/GoogleMapPage.py
@@ -17,7 +17,13 @@ with open("./data/raw/georef-switzerland-kanton.geojson") as response:
 # TODO: Remove duplications by uniqueID
 
 # Number of Migros stores per city
-reduced_df = df.groupby(by=['commune']).size().reset_index(name='count')
+reduced_df = df.groupby(by=['commune', 'is_migros']).size().reset_index(name='count')
+# store_count_df = reduced_df.set_index(['commune', 'is_migros'])['count'].unstack()
+# store_count_df = store_count_df.rename(columns={True: 'is_migros', False: 'is_competitor'})
+# store_count_df['is_migros'] = store_count_df['is_migros'].fillna(0).astype('int')
+# store_count_df['is_competitor'] = store_count_df['is_competitor'].fillna(0).astype('int')
+# store_count_df['total'] = store_count_df['is_migros'] + store_count_df['is_competitor']
+# st.dataframe(data=store_count_df)
 
 st.subheader("Plotting")
 
@@ -34,22 +40,49 @@ mapped_reduced_df = reduced_df.copy()
 mapped_reduced_df['canton'] = mapped_reduced_df['commune'].map(canton_dict)
 
 # Number of Migros stores per Canton
-mapped_reduced_df = mapped_reduced_df.groupby(by=['canton']).agg({
+mapped_reduced_df = mapped_reduced_df.groupby(by=['canton', 'is_migros']).agg({
     'count': 'sum'
 }).reset_index()
 
-# Add population
+store_count_df = mapped_reduced_df.set_index(['canton', 'is_migros'])['count'].unstack().reset_index()
+store_count_df = store_count_df.rename(columns={True: 'is_migros', False: 'is_competitor'})
+store_count_df['is_migros'] = store_count_df['is_migros'].fillna(0).astype('int')
+store_count_df['is_competitor'] = store_count_df['is_competitor'].fillna(0).astype('int')
+store_count_df['total'] = store_count_df['is_migros'] + store_count_df['is_competitor']
+mapped_reduced_df = store_count_df
+
+# Add population & some indicators to choose places to open new stores
 canton_population = pd.read_csv("./data/processed/canton_population.csv", low_memory=False)
-st.dataframe(data=canton_population)
-st.dataframe(data=mapped_reduced_df)
 mapped_reduced_df = mapped_reduced_df.join(canton_population.set_index('canton'), on='canton')
+mapped_reduced_df['popul_per_store'] = mapped_reduced_df['population'] / mapped_reduced_df['total']
+mapped_reduced_df['market_percent'] = mapped_reduced_df['is_migros'] * 100 / mapped_reduced_df['total']
     
 ## Plot against Canton
-st.text('Migros stores per Canton (after mapping) density')
+st.text('Population per store density')
+fig = px.choropleth_mapbox(
+    mapped_reduced_df,
+    geojson=geo_df_raw,
+    color="popul_per_store",
+    locations="canton",
+    featureidkey="properties.kan_name", #TODO: we should map regions/towns to city
+    center={"lat": 46.8, "lon": 8.3},
+    mapbox_style="carto-positron",
+    opacity=0.8,
+    width=800,
+    height=600,
+    hover_data=[],
+    zoom=6,
+    title='Migros Stores density in Switzerland', # <<< this doesn't make any changes!
+)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
+
+## Plot against Canton
+st.text('Market Percent per Canton')
 fig2 = px.choropleth_mapbox(
     mapped_reduced_df,
     geojson=geo_df_raw,
-    color="count",
+    color="market_percent",
     locations="canton",
     featureidkey="properties.kan_name", #TODO: we should map regions/towns to city
     center={"lat": 46.8, "lon": 8.3},
