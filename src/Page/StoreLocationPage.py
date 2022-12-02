286a3650518a5c9cb46c9030fe51bf6448f9bab1
Keon Nguyen <cuongmits@gmail.com>
2022-12-02 14:17:13 +0100

    Add Pie chart for Market Shares

--- src/Page/StoreLocationPage.py
+++ src/Page/StoreLocationPage.py
@@ -8,10 +8,10 @@ def load_data(path):
     df = pd.read_csv(path)
     return df
 
-df_raw = load_data(path='./data/processed/supermarkets_new.csv')
+df_raw = load_data(path='./data/git/supermarkets_last.csv')
 df = deepcopy(df_raw)
 
-st.subheader("Store Location of all Supermarket brands")
+st.subheader("Store Location of all Brands")
 fig = px.scatter_mapbox(
     df,
     lat='geometry.location.lat',
@@ -25,4 +25,11 @@ fig = px.scatter_mapbox(
     color= 'store_name')
 fig.update_layout(mapbox_style="carto-positron")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}, height=800)
+st.plotly_chart(fig)
+
+### another Pie chart
+
+st.subheader("Supermarket Share")
+sm_share=load_data(path='./data/git/supermarket_share.csv')
+fig = px.pie(sm_share, values='no_of_sm', names='store_name')
 st.plotly_chart(fig)
